0b74938b0a2487a47ad27bcfbfcf680f580c5cd8
rajansuman490 <anshumanraj490@gmail.com>
2024-10-23 12:22:49 +0900

    dist and package uploaded

--- dist/components/CustomDeleteModal.d.ts
+++ dist/components/CustomDeleteModal.d.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { ModalProps } from 'antd';
 interface CustomDeleteModalProps extends ModalProps {
     title: React.ReactNode;
-    content: string;
+    content: any;
 }
 declare const CustomDeleteModal: React.FC<CustomDeleteModalProps>;
 export default CustomDeleteModal;
